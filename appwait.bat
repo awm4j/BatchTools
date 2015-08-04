263dfc59398097585ef6f3d6fc97ae1f315a3250
awm4j <awm4j@users.noreply.github.com>
2015-08-04 12:23:24 -0400

    Create appwait.bat

---
+++ appwait.bat
@@ -0,0 +1,29 @@
+echo off
+
+if "%~1"=="help" goto :help
+
+SETLOCAL ENABLEEXTENSIONS
+SET process=%~1
+SET command=%~2
+
+echo [Waiting for process: %process%]
+
+:loop
+tasklist /FI "IMAGENAME eq %process%" /NH | find /I /N "%process%" > nul
+if "%ERRORLEVEL%"=="1" goto :exit
+timeout /t 1 /nobreak > nul
+goto :loop
+
+:help
+echo.
+echo.
+echo  appwait.bat [process] [command]
+echo.
+echo  Example:
+echo     appwait.bat iexplore.exe "shutdown -h"
+echo.
+goto :eof
+
+:exit
+timeout /t 2 /nobreak
+%command%
